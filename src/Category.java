6c3ca9255ead1302d84673517d61b0403274de43
progmatic-4 <harnorby@gmail.com>
2019-12-13 14:56:19 +0100

    category class

---
+++ src/Category.java
@@ -0,0 +1,17 @@
+public class Category {
+   private int id;
+    private String category;
+
+    public Category(int id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
